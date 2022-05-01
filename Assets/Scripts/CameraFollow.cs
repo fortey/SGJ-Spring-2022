a9159ba5d0330e5c75ea4cae93ddcd1ce9c1f369
fortey <fortey@inbox.ru>
2022-05-01 18:37:49 +0700

    camera direction

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using Cinemachine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+{
+    public float offsetX = 0.2f;
+    private CinemachineFramingTransposer _transposer;
+
+    void Start()
+    {
+        var vcam = GetComponent<CinemachineVirtualCamera>();
+        if (vcam != null)
+            _transposer = vcam.GetCinemachineComponent<CinemachineFramingTransposer>();
+    }
+
+    public void MoveLeft()
+    {
+        StartCoroutine(ChangeScreenX(0.5f + offsetX));
+    }
+    public void MoveRight()
+    {
+        StartCoroutine(ChangeScreenX(0.5f - offsetX));
+    }
+
+    IEnumerator ChangeScreenX(float newX)
+    {
+        float oldX = _transposer.m_ScreenX;
+        var time = 1f;
+        while (time > 0)
+        {
+            yield return new WaitForEndOfFrame();
+            time -= Time.deltaTime;
+            _transposer.m_ScreenX = Mathf.Lerp(newX, oldX, time);
+        }
+        _transposer.m_ScreenX = newX;
+    }
+}
