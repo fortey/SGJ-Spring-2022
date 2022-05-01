16a70835e19868454a5e692d5fd3c6381fbf3ac6
fortey <fortey@inbox.ru>
2022-05-01 11:19:47 +0700

    borders

---
+++ Assets/Scripts/BorderCheck.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BorderCheck : MonoBehaviour
+{
+    public event Action onBorderEnter;
+    public event Action onBorderExit;
+    private int _layerMask;
+    private readonly string _borderTag = "Border";
+
+    private void Start()
+    {
+        _layerMask = LayerMask.NameToLayer("Border");
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag(_borderTag))
+            onBorderEnter.Invoke();
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag(_borderTag))
+            onBorderExit.Invoke();
+    }
+
+}
