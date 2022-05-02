221fca9a38a05dae24f96f130c7f7c0f98f81e93
fortey <fortey@inbox.ru>
2022-05-03 00:37:17 +0700

    main menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,9 +3,17 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-
+    [SerializeField] private GameObject _bgName;
     public void StartGame()
     {
         SceneManager.LoadScene("fortey");
+    }
+
+    private void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            _bgName.SetActive(false);
+        }
     }
 }
