9c19bccb0e66f2d75c5a72b8d1fc1dffc2c38d8a
Shegarad <104271751+Shegarad@users.noreply.github.com>
2022-05-03 20:54:38 +0700

    menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,16 +4,43 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private GameObject _bgName;
+    [SerializeField] private GameObject _credits;
+
     public void StartGame()
     {
         SceneManager.LoadScene("fortey");
     }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+    public void credits()
+    {
+        _credits.SetActive(true);
+    }
+   
+    public void Start()
+    {
+        _credits.SetActive(false);
+    }
+
 
     private void Update()
     {
         if (Input.anyKeyDown)
         {
+            _credits.SetActive(false);
+        }
+        if (Input.anyKeyDown)
+        {
             _bgName.SetActive(false);
         }
+
+
+        if (Input.GetKey("escape"))
+        {
+                Application.Quit();
+        }
+        
     }
 }
